9e0a679f9828530fd1756ff9b53f0a64091f9f30
HAGA Shohei <shohaga@gmail.com>
2020-06-09 22:48:48 +0900

    update * add volume for velocity * add coninuos note on switch

--- src/VolumeUsbMidi/VolumeUsbMidi.ino
+++ src/VolumeUsbMidi/VolumeUsbMidi.ino
@@ -5,19 +5,35 @@
 Adafruit_USBD_MIDI usb_midi;
 MIDI_CREATE_INSTANCE(Adafruit_USBD_MIDI, usb_midi, MIDI);
 
-static const int kBufferSize = 8;
-
-static const int kNoteOffKey = 8;
-static const int kPitch = A7;
+static const int kNoteChannel = 1;
+static const int kFootSwitch = 8;
+static const int kPitch = A5;
 static const int kNoteNum = A6;
+static const int kNoteVel = A7;
+static const int kNoteType = 0;
+static const int kNoteOutNumBase = 60;
+static const int kNoteOutWidth = 40;
+static const uint32_t kUpdateSpanMillisec = 50;
+static const int kVelBase = 64;
+static const int kVelWidth = 40;
 
 static int beforeNote = -1;
-
+static int beforeVel = -1;
 static uint32_t last_millis = 0;
 static bool isNoteOff = true;
+static bool forceUpdate = false;
+
+static inline bool isNoteUpdate() {
+  return digitalRead(kNoteType) == LOW;
+}
+
+static inline bool isFootSwitchOn() {
+  return digitalRead(kFootSwitch) == LOW;
+}
 
 void initPeripheral() {
-  pinMode(kNoteOffKey, INPUT_PULLUP);
+  pinMode(kNoteType, INPUT_PULLUP);
+  pinMode(kFootSwitch, INPUT_PULLUP);
   analogReadResolution(14);
 }
 
@@ -29,35 +45,38 @@ void initMidi() {
   isNoteOff = true;
 }
 
-
 int getNoteValue() {
-  int value = analogRead(kNoteNum);
-  return (value >> 8) + 32;
+  double value = (double)(analogRead(kNoteNum) >> 2) - 2048;
+  return kNoteOutNumBase + (int) (value * kNoteOutWidth / 2048 + 0.5);
 }
 
 int getPitchValue() {
   return analogRead(kPitch) - 8192;
 }
 
+int getNoteVel() {
+  double value = (double)(analogRead(kNoteVel) >> 2) - 2048;
+  return kVelBase + (int) (value * kVelWidth / 2048 + 0.5);
+}
+
 void clearNote() {
   if (beforeNote < 0) return;
-//  MIDI.sendNoteOn(beforeNote, 0, 1);
-  MIDI.sendNoteOff(beforeNote, 64, 1);
-  //beforeNote = -1;
+  MIDI.sendNoteOff(beforeNote, beforeVel, kNoteChannel);
   isNoteOff = true;
 }
 
-void sendValue(int note, int pitch) {
-  if (!isNoteOff) {
-    MIDI.sendNoteOff(beforeNote, 64, 1);
+void sendValue(int note, int pitch, int vel, bool isForce) {
+  if (!isNoteOff && beforeNote >= 0 && (isForce || forceUpdate || note != beforeNote || vel != beforeVel)) {
+    MIDI.sendNoteOff(beforeNote, beforeVel, kNoteChannel);
   }
-  MIDI.sendPitchBend(pitch, 1);
-  //if (isNoteOff) {
-    MIDI.sendNoteOn(note, 64, 1);
-    isNoteOff = false;
-  //}
+  MIDI.sendPitchBend(pitch, kNoteChannel);
+  if (isForce || forceUpdate || note != beforeNote || vel != beforeVel) {
+    MIDI.sendNoteOn(note, vel, kNoteChannel);
+  }
+  isNoteOff = false;
   beforeNote = note;
-  
+  beforeVel = vel;
+  forceUpdate = isForce;
 }
 
 void setup() {
@@ -66,14 +85,17 @@ void setup() {
 }
 
 void loop() {
-  uint32_t c = millis();
-  if (last_millis > 0 && c - last_millis < 100) return;
-  last_millis = c;
-  if (digitalRead(kNoteOffKey) == LOW) {
+  if (isFootSwitchOn()) {
     clearNote();
-  } else  {
-    sendValue(getNoteValue(), getPitchValue());
   }
+
+  int m = millis();
+  if (!isNoteOff && last_millis > 0 && (m - last_millis) < kUpdateSpanMillisec) {
+    return;
+  }
+
+  last_millis = m;
+  sendValue(getNoteValue(), getPitchValue(), getNoteVel(), isNoteUpdate());
 
   MIDI.read();
 }
